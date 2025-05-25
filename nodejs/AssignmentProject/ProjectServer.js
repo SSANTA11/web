0f0e47e75e8b58db9cebfd3f0b37de1f8b0ce137
SSANTA11 <ssanta011205@skuniv.ac.kr>
2025-05-26 05:32:00 +0900

    Refactor ProjectServer to serve login.html, remove card.webp, and enhance study.html with card rotation effect and countdown timer. Add Room.html and SKU.svg files, and create basic login and project pages with Bootstrap styling.

--- nodejs/AssignmentProject/ProjectServer.js
+++ nodejs/AssignmentProject/ProjectServer.js
@@ -2,12 +2,12 @@
 let express = require("express"); // Express 모듈을 불러옴
 let path = require("path");
 let app = express();              // Express 애플리케이션 객체 생성
-let port=3000;
+let port = 3000;
 
 app.get('/',(req, res)=>{
-    res.sendFile(path.join(__dirname, "study.html"));
+    res.sendFile(path.join(__dirname, "login.html"));
 });
 
-app.listen(port, function(){      // 54222 포트에서 서버 실행
+app.listen(port, function(){
     console.log(`Server Running at http://localhost:${port}`); // 서버 실행 메시지 출력
 });
