ef1c51916d2e4d7f80d330ab349a882da0537a2b
SSANTA11 <ssanta011205@skuniv.ac.kr>
2025-05-26 15:36:46 +0900

    Add interactive card rotation feature with time and mouse position display

--- nodejs/AssignmentProject/ProjectServer.js
+++ nodejs/AssignmentProject/ProjectServer.js
@@ -1,13 +1,16 @@
 
-let express = require("express"); // Express 모듈을 불러옴
+let express = require("express");
 let path = require("path");
-let app = express();              // Express 애플리케이션 객체 생성
+let app = express();
 let port = 3000;
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/',(req, res)=>{
-    res.sendFile(path.join(__dirname, "login.html"));
+    res.sendFile(path.join(__dirname, 'public', "login.html"));
+});
+app.get('/main',(req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', "main.html"));
 });
 
 app.listen(port, function(){
-    console.log(`Server Running at http://localhost:${port}`); // 서버 실행 메시지 출력
+    console.log(`Server Running at http://localhost:${port}`);
 });
