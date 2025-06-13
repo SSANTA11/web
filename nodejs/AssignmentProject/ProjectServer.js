9d34e61d42761c8e458c0338226ef3f5de71b588
SSANTA11 <ssanta011205@skuniv.ac.kr>
2025-06-13 19:19:49 +0900

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- nodejs/AssignmentProject/ProjectServer.js
+++ nodejs/AssignmentProject/ProjectServer.js
@@ -1,18 +1,26 @@
-
 let express = require("express");
+
 let path = require("path");
+
 let bodyParser = require('body-parser');
+
 let app = express();
+
+let dot=['.','..','...'];
+let i =0;
 let port = 3000;
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.get('/',(req, res)=>{
+
+app.get('/login',(req, res)=>{
     res.sendFile(path.join(__dirname, 'public', "login.html"));
 });
 app.get('/main',(req, res)=>{
     res.sendFile(path.join(__dirname, 'public', "main.html"));
 });
+
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
   console.log("로그인 요청: ", username, password);
@@ -23,6 +31,16 @@ app.post('/login', (req, res) => {
     res.send("로그인 실패! 아이디나 비밀번호를 확인하세요.");
   }
 });
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  console.log(" 회원가입 요청: ", username, password);
+  senf
+});
+
 app.listen(port, function(){
-    console.log(`Server Running at http://localhost:${port}`);
+  let dots = setInterval(()=>{
+    process.stdout.write('\r서버 구동 중'+dot[i%dot.length]+'  ')
+    i++;
+  },500);
+  console.log(`http://localhost:${port}/login`);
 });
